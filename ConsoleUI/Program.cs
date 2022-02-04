614e5b6792a261690809eb7207c93dfa929d92ed
Beyza Nur Çetiner <190101036@ogrenci.yalova.edu.tr>
2022-02-04 17:21:08 +0300

    4/02/2022

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,19 +11,29 @@ namespace ConsoleUI
         {
 
             ProductManager productManager = new ProductManager(new EfProductDal());
-            
-            foreach (var product in productManager.GetProductDetails())
+
+            var result = productManager.GetProductDetails();
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName+ " / "+ product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + " / " + product.CategoryName);
 
 
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+            
 
             Console.WriteLine("************************************");
 
             ProductManager productManager2 = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2))
+            foreach (var product in productManager.GetAllByCategoryId(2).Data)
             {
                 Console.WriteLine(product.ProductName);
             }
