0213b6a0edaff687dbe6323c6c476de244dad6b7
Beyza Nur Çetiner <190101036@ogrenci.yalova.edu.tr>
2022-02-07 02:07:38 +0300

    7.02.2022

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -35,7 +35,7 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
 
-            if (DateTime.Now.Hour == 13)
+            if (DateTime.Now.Hour == 19)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
